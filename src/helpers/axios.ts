8a6580507a2bc5fbdb4e272928af9dc5d107036f
upsolve-git <160215136+upsolve-git@users.noreply.github.com>
2024-09-06 16:34:22 -0400

    Update axios.ts

--- src/helpers/axios.ts
+++ src/helpers/axios.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const customAxios = axios.create({
-  // baseURL: 'https://api-cgn.cypheryard.com',
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://api-cgn.cypheryard.com',
+  // baseURL: 'http://localhost:8000',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
